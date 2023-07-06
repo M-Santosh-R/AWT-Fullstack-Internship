044b48f27b9df04857314a2ac821dc52331b3a00
M-Santosh-R <137744326+M-Santosh-R@users.noreply.github.com>
2023-07-06 14:41:44 +0530

    Add files via upload

---
+++ DAY14/JSQ1.js
@@ -0,0 +1,4 @@
+function fade(){
+    $("#img1").fadeOut();
+    $("#img1".fadeTo("fast",0.1));
+}
---
+++ DAY14/JSQ2.js
@@ -0,0 +1,10 @@
+$("document").ready(function(){
+    $("#img1").mouseenter(function(){
+        $("#img1").css("width","700px");
+    });
+    
+    $("#img1").mouseleave(function(){
+        $("#img1").css("width","500px");
+    });
+    
+});
